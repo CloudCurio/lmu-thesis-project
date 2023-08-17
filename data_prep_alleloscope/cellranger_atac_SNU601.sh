1477f888d8ebc86b42893848b8adcbfc7212577b
Dmytro Hlushchenko <66057046+CloudCurio@users.noreply.github.com>
2023-08-17 15:46:34 +0200

    fixed the path

--- data_prep_alleloscope/cellranger_atac_SNU601.sh
+++ data_prep_alleloscope/cellranger_atac_SNU601.sh
@@ -5,7 +5,7 @@
 #SBATCH -p slim18 
 #SBATCH --mem-per-cpu 64000 
 
-/work/project/ladcol_005/tools/cellranger-atac-2.1.0/bin count --reference=/work/project/ladcol_014/thesis_cnvcalling/data/refdata-cellranger-arc-GRCh38-2020-A-2.0.0 --fastqs=/work/project/ladcol_014/thesis_cnvcalling/data/SNU601_scATACseq --sample=SRR12995620 --localcores 8 --localmem 64 --id=SNU601_atac
+/work/project/ladcol_005/tools/cellranger-atac-2.1.0/bin/cellranger-atac count --reference=/work/project/ladcol_014/thesis_cnvcalling/data/refdata-cellranger-arc-GRCh38-2020-A-2.0.0 --fastqs=/work/project/ladcol_014/thesis_cnvcalling/data/SNU601_scATACseq --sample=SRR12995620 --localcores 8 --localmem 64 --id=SNU601_atac
 
 
 
