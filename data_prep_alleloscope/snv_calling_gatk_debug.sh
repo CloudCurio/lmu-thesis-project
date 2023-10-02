6b77e3636445dfee9cc5872b9874c49581cf6171
Dmytro Hlushchenko <66057046+CloudCurio@users.noreply.github.com>
2023-10-02 13:10:28 +0200

    added a debug version of snv_calling_gatk.sh

--- data_prep_alleloscope/snv_calling_gatk_debug.sh
+++ data_prep_alleloscope/snv_calling_gatk_debug.sh
@@ -9,29 +9,29 @@ source /home/dhlushchenko/miniconda3/etc/profile.d/conda.sh
 conda activate thesis-env
 name=SNU601
 
-mkdir "./bam/"$name
-mkdir "./vcf/"$name
+mkdir "./bam_debug/"$name
+mkdir "./vcf_debug/"$name
 
 java -jar ../../tools/picard/picard.jar SortSam \
       I=possorted_new_header.bam \
-      O=bam/sorted_bam.bam \
+      O=bam_debug/sorted_bam.bam \
       SORT_ORDER=coordinate
 
 #rm.dup
 java -jar ../../tools/picard/picard.jar MarkDuplicates \
-I= "bam/sorted_bam.bam" \
-O= "./bam/"$name"/possorted_rmdup.bam" \
-M= "./bam/"$name"/possorted_rmdup_marked_dup_metics.txt" \
+I= "bam_debug/sorted_bam.bam" \
+O= "./bam_debug/"$name"/possorted_rmdup.bam" \
+M= "./bam_debug/"$name"/possorted_rmdup_marked_dup_metics.txt" \
 REMOVE_DUPLICATES=true \
 VALIDATION_STRINGENCY=LENIENT
 
 #sort
 module load ngs/samtools/1.9
-samtools sort -o "./bam/"$name"/possorted_rmdup_sort.bam" "./bam/"$name"/possorted_rmdup.bam"
+samtools sort -o "./bam_debug/"$name"/possorted_rmdup_sort.bam" "./bam_debug/"$name"/possorted_rmdup.bam"
 
 # build index
 java -jar ../../tools/picard/picard.jar BuildBamIndex \
-I= "./bam/"$name"/possorted_rmdup_sort.bam" \
+I= "./bam_debug/"$name"/possorted_rmdup_sort.bam" \
 VALIDATION_STRINGENCY=LENIENT
 
 
@@ -47,8 +47,8 @@ VALIDATION_STRINGENCY=LENIENT
 
 gatk --java-options "-Xmx4g" HaplotypeCaller \
 -R "/work/project/ladcol_004/epiAneufinder/ReferenceBWA/hg38_genome.fa" \
--I "./bam/"$name"/possorted_rmdup_sort.bam" \
--O "./vcf/"$name"/possorted_rmdup_sort.hc.vcf.gz"
+-I "./bam_debug/"$name"/possorted_rmdup_sort.bam" \
+-O "./vcf_debug/"$name"/possorted_rmdup_sort.hc.vcf.gz"
 
 ## decompress file
-gunzip "./vcf/"$name"/possorted_rmdup_sort.hc.vcf.gz"
+gunzip "./vcf_debug/"$name"/possorted_rmdup_sort.hc.vcf.gz"
