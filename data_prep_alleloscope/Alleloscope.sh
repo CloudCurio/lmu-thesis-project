2df62b15aff0e69e668a0dd2ba3831db74494228
Dmytro Hlushchenko <66057046+CloudCurio@users.noreply.github.com>
2023-10-30 15:14:23 +0100

    sourced conda

--- data_prep_alleloscope/Alleloscope.sh
+++ data_prep_alleloscope/Alleloscope.sh
@@ -5,6 +5,7 @@
 #SBATCH -p slim18 
 #SBATCH --mem-per-cpu 64000 
 
+source /home/dhlushchenko/miniconda3/etc/profile.d/conda.sh
 conda activate alleloscope
 
 Rscript ../../lmu-thesis-project/data_prep_alleloscope/Alleloscope.R
