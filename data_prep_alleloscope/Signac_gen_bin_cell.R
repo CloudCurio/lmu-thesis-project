83bedfbb84ba12eb8e622ee7056d5833bf520395
Dmytro Hlushchenko <66057046+CloudCurio@users.noreply.github.com>
2023-10-30 13:01:13 +0100

    segmentation table script added
    Also edits to Signac script(debug prints + matrix reformatting)

--- data_prep_alleloscope/Signac_gen_bin_cell.R
+++ data_prep_alleloscope/Signac_gen_bin_cell.R
@@ -5,11 +5,13 @@ chrom_sizes <- sizes_file$V2
 names(chrom_sizes) <- sizes_file$V1
 
 barcodes<-read.table("barcodes.tsv.gz")
-
+print("line 8 ok")
 tmp <- CreateFragmentObject(path="fragments.tsv.gz",
                             cells=barcodes$V1)
-
+print("line 11 ok")
 mtx <- GenomeBinMatrix(fragments = list(tmp),
                        genome = chrom_sizes,
                        binsize = 100000)
+print("line 15 ok")
+mtx <- as.matrix(mtx)
 write.table(mtx, file = "100k_fragments_sub.txt")
