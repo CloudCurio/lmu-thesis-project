1845f0b777aeedbb6448264243d8f6fd22d74251
Dmytro Hlushchenko <66057046+CloudCurio@users.noreply.github.com>
2023-10-30 10:41:55 +0100

    Update Signac_gen_bin_cell.R

--- data_prep_alleloscope/Signac_gen_bin_cell.R
+++ data_prep_alleloscope/Signac_gen_bin_cell.R
@@ -4,6 +4,11 @@ sizes_file <- read.table("./hg38/chrom.sizes.txt")
 chrom_sizes <- sizes_file$V2
 names(chrom_sizes) <- sizes_file$V1
 
+barcodes<-read.table("barcodes.tsv.gz")
+
+tmp <- CreateFragmentObject(path="fragments.tsv.gz",
+                            cells=barcodes$V1)
+
 mtx <- GenomeBinMatrix(fragments = "./fragments.tsv",
                        genome = chrom_sizes,
                        binsize = 100000)
