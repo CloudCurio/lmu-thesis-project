73c00e7df46687684bc32e9e59e676686fce5d81
Dmytro Hlushchenko <66057046+CloudCurio@users.noreply.github.com>
2023-10-30 14:50:28 +0100

    Create Alleloscope.sh

---
+++ data_prep_alleloscope/Alleloscope.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --error="alleloscope"%J".err"
+#SBATCH --output="alleloscope"%J".out"
+#SBATCH -J alleloscope
+#SBATCH -p slim18 
+#SBATCH --mem-per-cpu 64000 
+
+conda activate alleloscope
+
+Rscript ../../lmu-thesis-project/data_prep_alleloscope/Alleloscope.R
