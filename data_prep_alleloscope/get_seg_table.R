575cf3edd3ddbeec1265de010853e1a6306a6422
Dmytro Hlushchenko <66057046+CloudCurio@users.noreply.github.com>
2023-10-30 13:16:41 +0100

    Added comments for readability down the line

--- data_prep_alleloscope/get_seg_table.R
+++ data_prep_alleloscope/get_seg_table.R
@@ -1,3 +1,5 @@
+#This script extracts the segmentation table from an .rds file, provided by Alleloscope
+#To use, change the working directory and path to the object to the ones you need
 setwd("C:\\Users\\liber\\Desktop\\Study\\LMU\\Thesis Project - MariaCT's Lab\\Data")
 
 Obj_scDNA=readRDS("SNU601_dna.rds")
