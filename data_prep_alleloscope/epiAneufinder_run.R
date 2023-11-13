024595098ac23f9fbf7d28e81a51545d14067c70
Dmytro Hlushchenko <66057046+CloudCurio@users.noreply.github.com>
2023-11-13 15:35:03 +0100

    removed unneeded fread

--- data_prep_alleloscope/epiAneufinder_run.R
+++ data_prep_alleloscope/epiAneufinder_run.R
@@ -1,5 +1,5 @@
 library(epiAneufinder)
-epiAneufinder(input=fread("fragments.tsv.gz"), #Enter path to your fragments.tsv file or the folder containing bam files
+epiAneufinder(input="fragments.tsv.gz", #Enter path to your fragments.tsv file or the folder containing bam files
               outdir="..//..//output//epiAneufinder_results", #Path to the directory where results should be written 
               blacklist="hg38-blacklist.v2.bed", #Path to bed file that contains the blacklisted regions of your genome
               windowSize=5e5, 
