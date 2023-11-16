913d48ecf91dd757b9d4f1590f49fadde9efa0d6
Dmytro Hlushchenko <66057046+CloudCurio@users.noreply.github.com>
2023-11-16 10:54:29 +0100

    More tests

--- data_prep_alleloscope/Alleloscope_batch_run.R
+++ data_prep_alleloscope/Alleloscope_batch_run.R
@@ -1,7 +1,9 @@
 setwd("//work//project//ladcol_014//thesis_cnvcalling//lmu-thesis-project")
+#setwd("C:\\Users\\liber\\Desktop\\Study\\LMU\\Thesis Project - MariaCT's Lab\\Data")
 source("data_prep_alleloscope//Alleloscope_fun.R")
 
 seg_table_path <- "..//data//SNU601_scATACseq//seg_tables//seg_table_500k_epiAneuFinder_SNU601.rds"
+#seg_table_path <- "seg_table_500k_epiAneuFinder_SNU601.rds"
 
 input_table<-readRDS(seg_table_path)
 input_table<-input_table[,c("chr","start","end","length")]
@@ -11,10 +13,10 @@ for (chr in c(1)){
   alleloscope_run(dir_path = paste("//work//project//ladcol_014//thesis_cnvcalling//output//",
                                    "Alleloscope_batch//chr", chr, sep = ''),
                   seg_table = input_table[input_table$chr == chr,])
-  file.rename(from = paste("//work//project//ladcol_014//thesis_cnvcalling//output//Alleloscope_batch//chr", 
-                           chr, "//rds//theta_N_seg.rds", sep = ''), 
-              to = paste("//work//project//ladcol_014//thesis_cnvcalling//output//Alleloscope_batch//chr", 
-                         chr, "//theta_N_seg.rds", sep = ''))
+  # file.rename(from = paste("//work//project//ladcol_014//thesis_cnvcalling//output//Alleloscope_batch//chr", 
+  #                          chr, "//rds//theta_N_seg.rds", sep = ''), 
+  #             to = paste("//work//project//ladcol_014//thesis_cnvcalling//output//Alleloscope_batch//chr", 
+  #                        chr, "//theta_N_seg.rds", sep = ''))
   #unlink(paste("//work//project//ladcol_014//thesis_cnvcalling//output//Alleloscope_batch//chr", 
   #             chr, "//rds//", sep = ''), recursive = TRUE)
   #unlink(paste("//work//project//ladcol_014//thesis_cnvcalling//output//Alleloscope_batch//chr", 
