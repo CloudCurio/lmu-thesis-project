2cedeffa580ea5e646cf45b4387dfe23095468dc
Dmytro Hlushchenko <66057046+CloudCurio@users.noreply.github.com>
2023-12-06 09:54:37 +0100

    Rolled back the filtering change

--- data_prep_alleloscope/Alleloscope_fun.R
+++ data_prep_alleloscope/Alleloscope_fun.R
@@ -89,7 +89,7 @@ alleloscope_run <- function(wd = "//work//project//ladcol_014//thesis_cnvcalling
   print("starting segmentation")
   Obj_filtered$seg_table<-seg_table
   
-  Obj_filtered=Segments_filter(Obj_filtered=Obj_filtered, nSNP=100, len = 100000)
+  Obj_filtered=Segments_filter(Obj_filtered=Obj_filtered, nSNP=500, len = 100000)
   
   ################################################################################
   #Estimate cell major haplotype proportion for each region
