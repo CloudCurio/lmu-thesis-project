19324f2f215b5a5f5d02f92c0e115d8eaf99ecd9
Dmytro Hlushchenko <66057046+CloudCurio@users.noreply.github.com>
2023-12-07 14:08:33 +0100

    parameter change

--- data_prep_alleloscope/Alleloscope_fun.R
+++ data_prep_alleloscope/Alleloscope_fun.R
@@ -91,7 +91,7 @@ alleloscope_run <- function(wd = "//work//project//ladcol_014//thesis_cnvcalling
   Obj_filtered$seg_table<-seg_table
   
   Obj_filtered=Segments_filter(Obj_filtered=Obj_filtered, nSNP=100, len = 100000)
-  saveRDS(Obj_filtered, "100kb_obj.rds")
+  #saveRDS(Obj_filtered, "100kb_obj.rds")
   ################################################################################
   #Estimate cell major haplotype proportion for each region
   ################################################################################
