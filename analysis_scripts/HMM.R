7829956d3943e2872d21aea4997c45a8e3026936
Dmytro Hlushchenko <66057046+CloudCurio@users.noreply.github.com>
2024-01-16 13:03:04 +0100

    Distribution tests, improved model
    Now both response parameters (counts and theta-hat) are tested for goodness-of-fit to several distributions (poisson, gaussian and negative binomial). The new per-cell multinomial model is built with those tests in mind. Performance metrics are calculated for each model and summarized to give a view of overall fit to the multi-cell data.

--- analysis_scripts/HMM.R
+++ analysis_scripts/HMM.R
@@ -5,6 +5,10 @@
 library(depmixS4)
 library(dplyr)
 library(tidyr)
+library(fitdistrplus)
+library(MASS)
+library(caret)
+library(pROC)
 
 ###############################################################################
 # Load the input data
@@ -66,38 +70,162 @@ all_cells_data <- full_join(counts_long, theta_long, by = c("region", "cell"))
 all_cells_data <- full_join(all_cells_data, cnv_labels, by = "region")
 
 ###############################################################################
+# Explorative data analysis
+###############################################################################
+#poisson fit test
+fit_pois <- fitdist(dataset$counts, "pois")
+plotdist(dataset$counts, "pois", para = list(lambda = fit_pois$estimate))
+gofstat(fit_pois)
+
+#multinomial distribution
+fit_negbin <- fitdist(dataset$counts, "nbinom", 
+                      start = list(size = 1, mu = mean(dataset$counts)))
+plotdist(dataset$counts, "nbinom", 
+         para = list(size = fit_negbin$estimate[1], mu = fit_negbin$estimate[2]))
+gofstat(fit_negbin)
+
+#log gaussian
+log_data <- log(dataset$counts)
+fit_normal <- fitdist(log_data, "norm")
+plotdist(log_data, "norm", para = list(mean = fit_normal$estimate["mean"],
+                                       sd = fit_normal$estimate["sd"]))
+foo <- gofstat(fit_normal)
+ks_result <- ks.test(log_data, "pnorm", mean = fit_normal$estimate["mean"], 
+                     sd = fit_normal$estimate["sd"])
+
+#class-by-class tests
+dataset_base <- dataset[which(dataset$wgs_cnv_score<2.5),]
+dataset_gain <- dataset[which(dataset$wgs_cnv_score>=2.5),]
+
+#poisson
+fit_pois <- fitdist(dataset_base$counts, "pois")
+plotdist(dataset_base$counts, "pois", para = list(lambda = fit_pois$estimate))
+gofstat(fit_pois)
+
+fit_pois <- fitdist(dataset_gain$counts, "pois")
+plotdist(dataset_gain$counts, "pois", para = list(lambda = fit_pois$estimate))
+gofstat(fit_pois)
+
+#neg.binom.
+fit_negbin <- fitdist(dataset_base$counts, "nbinom", 
+                      start = list(size = 1, mu = mean(dataset_base$counts)))
+plotdist(dataset_base$counts, "nbinom", 
+         para = list(size = fit_negbin$estimate[1], mu = fit_negbin$estimate[2]))
+gofstat(fit_negbin)
+
+fit_negbin <- fitdist(dataset_gain$counts, "nbinom", 
+                      start = list(size = 1, mu = mean(dataset_gain$counts)))
+plotdist(dataset_gain$counts, "nbinom", 
+         para = list(size = fit_negbin$estimate[1], mu = fit_negbin$estimate[2]))
+gofstat(fit_negbin)
+###############################################################################
 # Construct an HMM
 ###############################################################################
 
 set.seed(2024)
 
 #build a simple univariate model
-train_mod <- depmix(response = counts ~ 1, data = dataset, nstates = 2,
-                     family = poisson())
-f_train <- fit(train_mod, verbose = TRUE, emc = em.control(rand = FALSE))
-print(f_train)
-summary(f_train)
+# train_mod <- depmix(response = counts ~ 1, data = dataset, nstates = 2,
+#                      family = gaussian("log"))
+# fm <- fit(train_mod, verbose = TRUE, emc = em.control(rand = FALSE))
+# print(fm)
+# summary(fm)
 
 #predict states for new data and estimate accuracy
-cells <- unique(all_cells_data$cell)
-cells <- cells[-which(cells == best_cell)]
-set.seed(2024)
-test_set <- all_cells_data[which(all_cells_data$cell == sample(cells, 1)),]
-set.seed(2024)
-test_mod <- depmix(response = counts ~ 1, data = test_set, nstates = 2,
-                   family = poisson())
-test_mod <- setpars(test_mod, getpars(f_train))
+# cells <- unique(all_cells_data$cell)
+# cells <- cells[-which(cells == best_cell)]
+# set.seed(2024)
+# test_set <- all_cells_data[which(all_cells_data$cell == sample(cells, 1)),]
+# set.seed(2024)
+# test_mod <- depmix(response = counts ~ 1, data = test_set, nstates = 2,
+#                    family = poisson())
+# test_mod <- setpars(test_mod, getpars(f_train))
 
-#try a multivariate model with gaussian distributions
-multiV_mod <- depmix(response = list(counts ~ 1, theta_hat ~ 1), data = all_cells_data, 
-                     nstates = 3, family = list(gaussian("identity"), multinomial()))
-f_mV <- fit(multiV_mod, verbose = TRUE)
-print(f_mV)
-summary(f_mV)
+#create a dataframe for metrics
+metrics <- data.frame(matrix(ncol = 9, nrow = length(unique(all_cells_data$cell))))
+colnames(metrics) <- c("Accuracy", "Accuracy P-value", "Kappa", "Sensitivity", 
+                       "Specificity", "Precision", "AUC", "region_count", "NA_theta")
+rownames(metrics) <- unique(all_cells_data$cell)
 
-#try a multivariate model with tailored distributions
-mV_tuned_mod <- depmix(response = list(counts ~ 1, theta_hat ~ 1), data = dataset, 
-                     nstates = 3, family = list(poisson(), gaussian()))
-f_mVt <- fit(mV_tuned_mod, verbose = TRUE)
-print(f_mVt)
-summary(f_mVt)
+#build a multivariate model for each cell
+for (cell in unique(all_cells_data$cell)){
+  set.seed(2024)
+  data_sub <- all_cells_data[all_cells_data$cell == cell,]
+  
+  #record library size and missing theta-hat count
+  metrics[cell, "region_count"] <- nrow(data_sub)
+  metrics[cell, "NA_theta"] <- sum(is.na(data_sub$theta_hat))
+  
+  mod <- depmix(response = list(counts ~ 1, theta_hat ~ 1), data = data_sub, 
+                       nstates = 2, family = list(gaussian("log"), gaussian("identity")))
+  fm <- fit(mod, verbose = TRUE)
+  # print(fm)
+  # summary(fm)
+  
+  #determine which class is which CNV
+  response_mtx <- summary(fm, which = "response")
+  base_class <- ifelse(response_mtx[1, "Re1.(Intercept)"] < response_mtx[2, "Re1.(Intercept)"],
+                       1,2)
+  gain_class <- ifelse(base_class == 1, 2, 1)
+  
+  #create prediction and reference vectors
+  true_classes <- ifelse(data_sub$wgs_score<2.5, base_class, gain_class)
+  pred <- posterior(fm)[,1]
+  
+  #calculate performance metrics
+  #create a confusion matrix and extract the metrics
+  confmat <- confusionMatrix(data = as.factor(pred), reference <- as.factor(true_classes))
+  
+  metrics[cell, "Accuracy"] <- confmat$overall["Accuracy"]
+  metrics[cell, "Accuracy P-value"] <- confmat$overall["AccuracyPValue"]
+  metrics[cell, "Kappa"] <- confmat$overall["Kappa"]
+  metrics[cell, "Sensitivity"] <- confmat$byClass["Sensitivity"]
+  metrics[cell, "Specificity"] <- confmat$byClass["Specificity"]
+  metrics[cell, "Precision"] <- confmat$byClass["Precision"]
+  
+  #build a ROC curve
+  roc_curve <- roc(true_classes, pred)
+  metrics[cell, "AUC"] <- auc(roc_curve)[1]
+}
+
+#get metric summaries
+for (metric in colnames(metrics)){
+  print(metric)
+  print(summary(metrics[,metric]))
+}
+
+#get metric distributions
+for (metric in colnames(metrics)){
+  hist(metrics[,metric], main = metric, xlab = metric)
+}
+
+#evaluate epiAneufinder performance for comparison
+epiAneufinder_preds <- ifelse(cnv_labels$cnv == 2, base_class, gain_class)
+confmat_epiA <- confusionMatrix(data = as.factor(epiAneufinder_preds), reference <- as.factor(true_classes))
+print(confmat_epiA$overall["Accuracy"])
+print(confmat_epiA$overall["AccuracyPValue"])
+print(confmat_epiA$overall["Kappa"])
+print(confmat_epiA$byClass["Sensitivity"])
+print(confmat_epiA$byClass["Specificity"])
+print(confmat_epiA$byClass["Precision"])
+
+roc_curve <- roc(true_classes, epiAneufinder_preds)
+print("AUC")
+print(auc(roc_curve)[1])
+
+#check for correlation between metrics and the amount of missing theta hat values
+correlations <- cor(metrics)[, "NA_theta"]
+print(correlations)
+
+###############################################################################
+# Test accuracy
+###############################################################################
+
+true_classes <- ifelse(dataset$wgs_cnv_score<2.5, 1, 2)
+pred <- posterior(fm)[,1]
+roc_curve <- roc(true_classes, pred)
+plot(roc_curve)
+auc(roc_curve)
+
+confmat <- confusionMatrix(data = as.factor(pred), reference <- as.factor(true_classes))
+confmat
