69bfc7c098344c137cd1b6b80643d10cc4dd1096
Dmytro Hlushchenko <66057046+CloudCurio@users.noreply.github.com>
2024-01-23 10:13:51 +0100

    changed window size

--- data_prep_alleloscope/epiAneufinder_run.R
+++ data_prep_alleloscope/epiAneufinder_run.R
@@ -2,7 +2,7 @@ library(epiAneufinder)
 epiAneufinder(input="fragments.tsv.gz", #Enter path to your fragments.tsv file or the folder containing bam files
               outdir="..//..//output//epiAneufinder_results", #Path to the directory where results should be written 
               blacklist="hg38-blacklist.v2.bed", #Path to bed file that contains the blacklisted regions of your genome
-              windowSize=5e5, 
+              windowSize=1e6, 
               genome="BSgenome.Hsapiens.UCSC.hg38", #Substitute with relevant BSgenome
               exclude=c('chrX','chrY','chrM'), 
               reuse.existing=TRUE,
