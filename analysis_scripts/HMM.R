633154e1bd4a576ee229fc05b0d6ebc33af255a6
Dmytro Hlushchenko <66057046+CloudCurio@users.noreply.github.com>
2024-01-30 15:07:25 +0100

    Changes in paths, metrics visualization and other
    1) Unified the paths for inputs and outputs for easier switching between datasets
    2) Added an option to run the HMM on counts only or on theta-hat and counts combined
    3) Created a series of dataframe templates for easier metrics storage and change
    4) Implemented 3-class capability
    5) Revamped metrics visualization and analysis, fixed the outputs so that the results are stored properly
    6) Implemented epiAneufinder per-cell analysis and comparison with HMM metrics

--- analysis_scripts/HMM.R
+++ analysis_scripts/HMM.R
@@ -17,13 +17,17 @@ library(ggpubr)
 
 #TODO: start substituting paths with variables for further conversion into a function
 stat_flag <- FALSE
-models_to_test <- "filtered" #options: "full", "filtered", c("full", "filtered")
+#options: "counts", "counts_and_theta", c("counts", "counts_and_theta")
+models_to_test <- c("counts", "counts_and_theta") 
+
+input_dir <- "C:\\Users\\liber\\Desktop\\Study\\LMU\\Thesis Project - MariaCT's Lab\\Data\\HMM inputs\\Alleloscope_1mb_batch"
+output_dir <- "..//..//HMM outputs//Alleloscope_1mb_batch//"
 
 ###############################################################################
 # Load the input data
 ###############################################################################
 
-setwd("C:\\Users\\liber\\Desktop\\Study\\LMU\\Thesis Project - MariaCT's Lab\\Data\\HMM inputs\\Alleloscope_batch_fixed")
+setwd(input_dir)
 
 #load theta vals (full and filtered)
 theta_vals_full <- read.csv("HMM_theta_full.csv", row.names = 1)
@@ -138,7 +142,8 @@ if (stat_flag == TRUE){
                        sd = fit_normal$estimate["sd"])
   
   #class-by-class tests
-  dataset_base <- dataset[which(dataset$wgs_cnv_score<2.5),]
+  dataset_loss <- dataset[which(dataset$wgs_cnv_score<=1.5),]
+  dataset_base <- dataset[which(dataset$wgs_cnv_score<2.5) && which(dataset$wgs_cnv_score>1.5),]
   dataset_gain <- dataset[which(dataset$wgs_cnv_score>=2.5),]
   
   #poisson
@@ -207,23 +212,35 @@ set.seed(2024)
 #create a list for storage of models
 models <- replicate(length(models_to_test), list(), simplify = FALSE)
 names(models) <- models_to_test
-if ("filtered" %in% models_to_test){
-  models$filtered[["data"]] <- all_cells_data_filtered
-}
-if ("full" %in% models_to_test){
-  models$full[["data"]] <- all_cells_data_full
+for (model in names(models)){
+  if (model == "full"){
+    models[[model]][["data"]] <- all_cells_data_full
+  } else {
+    models[[model]][["data"]] <- all_cells_data_filtered
+  }
 }
 
 #train HMM models and calculate performance metrics
 for (model in names(models)){
   #create a list for models
   models[[model]][["fitted_models"]] <- list()
-  #create a dataframe for metrics
-  metrics <- data.frame(matrix(ncol = 9, nrow = length(unique(models[[model]][["data"]]$cell))))
-  colnames(metrics) <- c("Accuracy", "Accuracy P-value", "Kappa", "Sensitivity",
-                         "Specificity", "Precision", "AUC", "region_count", "NA_theta")
-  rownames(metrics) <- unique(models[[model]][["data"]]$cell)
-  models[[model]][["metrics"]] <- metrics
+
+  #create dataframes for metrics storage
+  overall_metrics_df <- data.frame(matrix(ncol = 3, nrow = length(unique(models[[model]][["data"]]$cell))))
+  colnames(overall_metrics_df) <- c("overall_Acc", "NA_theta", "region_count")
+  rownames(overall_metrics_df) <- unique(models[[model]][["data"]]$cell)
+  
+  per_class_metrics_df <- data.frame(matrix(ncol = 12, nrow = length(unique(models[[model]][["data"]]$cell))))
+  colnames(per_class_metrics_df) <- c("Sensitivity", "Specificity", "Pos Pred Value",
+                                      "Neg Pred Value", "Precision", "Recall", "F1",
+                                      "Prevalence", "Detection Rate", "Detection Prevalence",
+                                      "Balanced Accuracy", "AUC")
+  rownames(per_class_metrics_df) <- unique(models[[model]][["data"]]$cell)
+  
+  #create a list for metrics storage
+  metrics_list <- list("loss" = per_class_metrics_df, "base" = per_class_metrics_df, 
+                       "gain" = per_class_metrics_df, "overall" = overall_metrics_df)
+  models[[model]][["metrics"]] <- metrics_list
   
   #create a dataframe for predictions
   HMM_preds <- data.frame(fragment = character(0), cell = character(0), HMM_pred = numeric(0), WGS_score = numeric(0))
@@ -235,60 +252,79 @@ for (model in names(models)){
     
     #skip the cell if there are less than 5 non-NA theta-hat values present for it
     #TODO: figure out why different models break at different non_NA theta-hat numbers
-    if (sum(!is.na(data_sub$theta_hat))>=5){
-      #record library size and missing theta-hat count
-      metrics[cell, "region_count"] <- nrow(data_sub)
-      metrics[cell, "NA_theta"] <- sum(is.na(data_sub$theta_hat))
-      
+    # if (sum(!is.na(data_sub$theta_hat))>=5){}
+    
+    #record library size and missing theta-hat count
+    metrics_list[["overall"]][cell, "region_count"] <- nrow(data_sub)
+    metrics_list[["overall"]][cell, "NA_theta"] <- sum(is.na(data_sub$theta_hat))
+    
+    #choose a model for training
+    if (model == "counts"){
+      mod <- depmix(response = counts ~ 1, data = data_sub, 
+                    nstates = 3, family = gaussian("log"))
+    } else {
       mod <- depmix(response = list(counts ~ 1, theta_hat ~ 1), data = data_sub, 
-                    nstates = 2, family = list(gaussian("log"), gaussian("identity")))
-      print(paste("cell:", cell, "model", model))
-      fm <- fit(mod, verbose = FALSE)
-      models[[model]][["fitted_models"]][[cell]] <- fm
-      # print(fm)
-      # summary(fm)
-      
-      #determine which class is which CNV
-      response_mtx <- summary(fm, which = "response")
-      #TODO: when we have 3 classes, change the label assigment to include losses and do it through min/max functions
-      #loss_class <- which(response_mtx[,"Re1.(Intercept)"] == min(response_mtx[,"Re1.(Intercept)"]))
-      base_class <- ifelse(response_mtx[1, "Re1.(Intercept)"] < response_mtx[2, "Re1.(Intercept)"],
-                           1,2)
-      gain_class <- ifelse(base_class == 1, 2, 1)
-      
-      #create prediction and reference vectors
-      #TODO: add class assignment for 3 classes when we have losses in the data
-      true_classes <- ifelse(data_sub$wgs_score<2.5, base_class, gain_class)
-      pred <- posterior(fm)[,1]
-      
-      #map predictions to the HMM_preds
-      insert_df <- data_sub[, c("region", "cell")]
-      insert_df$HMM_pred <- ifelse(pred == base_class, 2,3)
-      insert_df$WGS_score <- ifelse(data_sub$wgs_score<2.5, 2, 3)
-      HMM_preds <- rbind(HMM_preds, insert_df)
-      
-      #calculate performance metrics
-      #create a confusion matrix and extract the metrics
-      confmat <- confusionMatrix(data = as.factor(pred), reference <- as.factor(true_classes))
-      
-      metrics[cell, "Accuracy"] <- confmat$overall["Accuracy"]
-      metrics[cell, "Accuracy P-value"] <- confmat$overall["AccuracyPValue"]
-      metrics[cell, "Kappa"] <- confmat$overall["Kappa"]
-      metrics[cell, "Sensitivity"] <- confmat$byClass["Sensitivity"]
-      metrics[cell, "Specificity"] <- confmat$byClass["Specificity"]
-      metrics[cell, "Precision"] <- confmat$byClass["Precision"]
+                    nstates = 3, family = list(gaussian("log"), gaussian("identity")))
+    }
+    print(paste("cell:", cell, "model:", model))
+    fm <- fit(mod, verbose = FALSE)
+    models[[model]][["fitted_models"]][[cell]] <- fm
+    # print(fm)
+    # summary(fm)
+    
+    #determine which class is which CNV
+    response_mtx <- summary(fm, which = "response")
+    #TODO: when we have 3 classes, change the label assigment to include losses and do it through min/max functions
+    loss_class <- which(response_mtx[,"Re1.(Intercept)"] == min(response_mtx[,"Re1.(Intercept)"]))
+    gain_class <- which(response_mtx[,"Re1.(Intercept)"] == max(response_mtx[,"Re1.(Intercept)"]))
+    base_class <- which(!(c(1,2,3) %in% c(loss_class, gain_class)))
+    
+    #'@two_state_relabeling
+    # base_class <- ifelse(response_mtx[1, "Re1.(Intercept)"] < response_mtx[2, "Re1.(Intercept)"],
+    #                      1,2)
+    # gain_class <- ifelse(base_class == 1, 2, 1)
+    
+    #create prediction and reference vectors
+    true_classes <- ifelse(data_sub$wgs_score >= 2.5, gain_class, ifelse(data_sub$wgs_score <= 1.5, loss_class, base_class))
+    pred <- posterior(fm)[,1]
+    
+    #map predictions to the HMM_preds
+    insert_df <- data_sub[, c("region", "cell")]
+    insert_df$HMM_pred <- ifelse(pred == loss_class, 1, ifelse(pred == base_class, 2, 3))
+    insert_df$WGS_score <- ifelse(data_sub$wgs_score<=1.5, 1, ifelse(data_sub$wgs_score>=2.5, 3, 2))
+    HMM_preds <- rbind(HMM_preds, insert_df)
+    
+    #create a confusion matrix and extract the metrics
+    confmat <- confusionMatrix(data = as.factor(pred), reference <- as.factor(true_classes))
+    for (cnv_type in c(1:3)){
+      #extract the metrics into a single-row dataframe
+      extracted_metrics <- confmat$byClass[cnv_type,]
       
       #build a ROC curve
-      roc_curve <- roc(true_classes, pred)
-      metrics[cell, "AUC"] <- auc(roc_curve)[1]
+      binary_ref <- ifelse(true_classes == cnv_type, 1, 0)
+      binary_pred <- ifelse(pred == cnv_type, 1, 0)
+      roc_curve <- roc(binary_ref, binary_pred)
+      
+      #add AUC as a column to the extracted_metrics df
+      extracted_metrics$AUC <- auc(roc_curve)[1]
+      
+      #add the metrics as a row to the according dataframe in the model storage list
+      metrics_list[[cnv_type]][cell, ] <- extracted_metrics
     }
+    metrics_list[["overall"]][cell, "overall_Acc"] <- confmat$overall[["Accuracy"]]
   }
   models[[model]][["predictions"]] <- HMM_preds
-  models[[model]][["metrics"]] <- metrics
+  models[[model]][["metrics"]] <- metrics_list
   
   #get metric summaries
-  models[[model]][["metrics_summary"]] <- summary(metrics)
+  models[[model]][["metrics_summary"]] <- list("loss" = NA, "base" = NA, "gain" = NA)
+  for (cnv_type in names(models[[model]][["metrics_summary"]])){
+    models[[model]][["metrics_summary"]][[cnv_type]] <- summary(models[[model]][["metrics"]][[cnv_type]])
+  }
 }
+
+#save the models list as an RDS
+saveRDS(models, paste(output_dir, "models.RDS", sep = ""))
 
 # #build a multivariate model for each cell
 # for (cell in unique(all_cells_data$cell)){
@@ -343,42 +379,169 @@ for (model in names(models)){
 # write.csv(summary(metrics), "..//..//HMM outputs//Alleloscope_old_epiA_batch//metrics_summary.csv")
 
 ###############################################################################
-# Test accuracy
+# Evaluate model metrics and save their distributions as plots
 ###############################################################################
 
-#get metric distributions
-for (metric in names(metrics)){
-  plot_data <- data.frame()
-  for (model in names(models)){
-    plot_data[model] <- models[[model]][["metrics"]]$metric
+#check for correlation between metrics and the amount of missing theta hat values
+for (model in names(models)){
+  #create an output dataframe
+  corr_df <- data.frame(matrix(ncol = 15, nrow = 3))
+  rownames(corr_df) <- c("loss", "base", "gain")    #TODO: change if more than one model will be implemented
+  
+  for (cnv_type in names(models[[model]][["metrics"]])[-4]){
+    print(paste("Correlation between metrics and missing theta-values for the", cnv_type, "class (", model, "model)"))
+    metrics_merged <- models[[model]][["metrics"]][[cnv_type]]
+    metrics_merged <- cbind(models[[model]][["metrics"]][[cnv_type]], models[[model]][["metrics"]][["overall"]])
+    correlations <- cor(metrics_merged)[, "NA_theta"]
+    corr_df[cnv_type,] <- correlations
+    print(correlations)
   }
+  #save the output dataframe
+  colnames(corr_df) <- names(correlations)
+  write.csv(corr_df, paste(output_dir, model, "_", "metrics_correlation_with_NA_theta.csv", sep = ""))
 }
 
+
 #get metric distributions
-pdf(file <- "..//..//HMM outputs//Alleloscope_old_epiA_batch//metrics_distributions.pdf")
-for (metric in colnames(metrics)){
-  hist(metrics[,metric], main = metric, xlab = metric)
+#for per_class metrics
+pdf(paste(output_dir, "metrics_distributions_per_class.pdf", sep = ""))
+for (metric in names(metrics_list[["loss"]])){
+  for (model in names(models)){
+    for (cnv_class in c("loss", "base", "gain")){
+      print(paste(metric, model))
+      plot_data <- models[[model]][["metrics"]][[cnv_class]][,metric]
+      
+      par(mar = c(5.1, 4.1, 4.1, 8.1))
+      hist(plot_data, breaks = 10, main = paste(metric, "distribution for", model, 
+                                                "model", paste("(", cnv_class, ")", sep = "")), 
+           xlab = metric)
+      summarized_data <- summary(plot_data)
+      summarized_data <- paste(names(summarized_data), round(summarized_data, 3), sep = ":")
+      legend("topright", legend = summarized_data, xpd = TRUE, inset = c(-0.3, 0))
+    }
+  }
 }
 dev.off()
 
+#for overall metrics
+pdf(paste(output_dir, "metrics_distributions_overall.pdf", sep = ""))
+for (metric in names(metrics_list[["overall"]])){
+  for (model in names(models)){
+    plot_data <- models[[model]][["metrics"]][["overall"]][, metric]
+    
+    par(mar = c(5.1, 4.1, 4.1, 8.1))
+    hist(plot_data, breaks = 10, main = paste(metric, "distribution for", model, 
+                                              "model"), 
+         xlab = metric)
+    summarized_data <- summary(plot_data)
+    summarized_data <- paste(names(summarized_data), round(summarized_data, 3), sep = ":")
+    legend("topright", legend = summarized_data, xpd = TRUE, inset = c(-0.3, 0))
+  }
+}
+dev.off()
+
+###############################################################################
+# Test epiAneufinder accuracy
+###############################################################################
+
 #evaluate epiAneufinder performance for comparison
-epiAneufinder_preds <- ifelse(cnv_labels$cnv == 2, base_class, gain_class)
-confmat_epiA <- confusionMatrix(data = as.factor(epiAneufinder_preds), reference <- as.factor(true_classes))
+#create a discretized ground truth vector
+epiA_truth <- ifelse(cnv_labels_pb$wgs_score <= 1.5, 1, ifelse(cnv_labels_pb$wgs_score >= 2.5, 3, 2))
+confmat_epiA <- confusionMatrix(data = as.factor(cnv_labels_pb$cnv), 
+                                reference = as.factor(epiA_truth))
+
 print(confmat_epiA$overall["Accuracy"])
-print(confmat_epiA$overall["AccuracyPValue"])
-print(confmat_epiA$overall["Kappa"])
-print(confmat_epiA$byClass["Sensitivity"])
-print(confmat_epiA$byClass["Specificity"])
-print(confmat_epiA$byClass["Precision"])
+print(confmat_epiA$byClass)
 
-roc_curve <- roc(true_classes, epiAneufinder_preds)
-print("AUC")
-print(auc(roc_curve)[1])
+#save results in an output dataframe
+epiA_eval_df <- as.data.frame(confmat_epiA$byClass)
+rownames(epiA_eval_df) <- c("loss", "base", "gain")
+epiA_eval_df$overall_Acc <- confmat_epiA$overall["Accuracy"]
 
-#check for correlation between metrics and the amount of missing theta hat values
-correlations <- cor(metrics)[, "NA_theta"]
-print(correlations)
+#build ROC curves
+epiA_eval_df$AUC <- NA
+for (cnv_type in c(1:3)){
+  binary_ref <- ifelse(epiA_truth == cnv_type, 1, 0)
+  binary_pred <- ifelse(cnv_labels_pb$cnv == cnv_type, 1, 0)
+  roc_curve <- roc(binary_ref, binary_pred)
+  
+  #add AUC as a column to the output df
+  print(paste("AUC for", c("loss", "base", "gain")[cnv_type], "class"))
+  print(auc(roc_curve)[1])
+  
+  epiA_eval_df$AUC[cnv_type] <- auc(roc_curve)[1]
+}
 
+write.csv(epiA_eval_df, paste(output_dir, "epiAneufinder_pseudobulk_metrics.csv", sep = ""))
+
+#calculate metrics per-cell predictions
+#subset the per-cell predictions to only include regions, present in Alleloscope output and cells with enough theta hat values
+cnv_labels_per_cell_filtered <- cnv_labels_per_cell[rownames(cnv_labels_per_cell) %in% rownames(cnv_labels_pb), 
+                                                    colnames(cnv_labels_per_cell) %in% all_cells_data_filtered$cell]
+
+#create an output dataframe
+epiA_eval <- data.frame(matrix(ncol = ncol(per_class_metrics_df) + 3,
+                               nrow = 0))
+colnames(epiA_eval) <- c(colnames(per_class_metrics_df), "overall_Acc", "cell", "cnv_class")
+
+#create a confusion matrix for each cell and save metrics as a dataframe
+for (cell in colnames(cnv_labels_per_cell_filtered)){
+  epiA_pred <- cnv_labels_per_cell_filtered[, cell]
+  confmat_epiA <- confusionMatrix(data = as.factor(epiA_pred),
+                                  reference = as.factor(epiA_truth))
+  for (cnv_class in (c("loss", "base", "gain"))){
+    #extract confusion matrix metrics
+    extracted_metrics <- as.data.frame(t(confmat_epiA$byClass[which(c("loss", "base", "gain") == cnv_class),]))
+    extracted_metrics$overall_Acc <- confmat_epiA$overall[["Accuracy"]]
+    
+    #build ROC curves
+    binary_ref <- ifelse(epiA_truth == which(c("loss", "base", "gain") == cnv_class), 1, 0)
+    binary_pred <- ifelse(epiA_pred == which(c("loss", "base", "gain") == cnv_class), 1, 0)
+    roc_curve <- roc(binary_ref, binary_pred)
+    
+    #add AUC as a column to the output df
+    extracted_metrics$AUC <- auc(roc_curve)[1]
+    
+    #save cell and cnv_type
+    extracted_metrics$cell <- cell
+    extracted_metrics$cnv_class <- cnv_class
+    
+    #save metrics to the output dataframe
+    epiA_eval <- rbind(epiA_eval, extracted_metrics) 
+  }
+}
+#save the output dataframe
+
+write.csv(epiA_eval, paste(output_dir, "epiAneufinder_per_cell_metrics.csv", sep = ""))
+
+
+#plot epiAneufinder per-cell metrics against HMM metrics
+#create a storage list for plots
+
+pdf(paste(output_dir, "epiAneufinder_and_HMM_metrics_comparison.pdf", sep = ""))
+for (cnv_type in c("loss", "base", "gain")){
+  for (model in names(models)){
+    #subset the data by cnv type and model name
+    data_sub <- models[[model]][["metrics"]][[cnv_type]]
+    epiA_sub <- epiA_eval[(epiA_eval$cnv_class == cnv_type),]
+    #plot each metric
+    for (metric in colnames(data_sub)){
+      #create a dataframe for plotting
+      plot_data <- data.frame(value = c(data_sub[, metric], epiA_sub[, metric]),
+                              source_model = rep(c("HMM", "epiAneufinder"), each = nrow(data_sub)))
+      # create a ggplot object
+      p <- ggplot(plot_data, aes(value, fill = source_model)) + 
+        geom_histogram(position = "identity", alpha = 0.5, colour = "black", bins = 30) +
+        scale_fill_brewer(palette = "Set1") +
+        theme_minimal() +
+        labs(title = paste(metric, "distribution for the ", model, "HMM model and epiAneudinder \n",
+                           paste("(", cnv_type, ")", sep = "")), 
+             x = metric, y = "Count")
+      print(p)
+    }
+  }
+}
+dev.off()
 ###############################################################################
 # Plot the results on a karyogram
 ###############################################################################
